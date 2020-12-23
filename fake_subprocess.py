0248971901129b52ee359f2eb7a7587f3594409b
Frankie Robertson <frankie@robertson.name>
2020-12-23 12:31:37 +0200

    Fixup

--- fake_subprocess.py
+++ fake_subprocess.py
@@ -5,24 +5,28 @@ class AbsoluteImport:
         if sys.path[0] == "":
             self.removed_sys_path = True
             del sys.path[0]
+        self.saved_subprocess = None
+        if "subprocess" in sys.modules:
+            self.saved_subprocess = sys.modules["subprocess"]
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         import sys
         if self.removed_sys_path:
             sys.path.insert(0, "")
+        if self.saved_subprocess is not None:
+            sys.modules["subprocess"] = self.saved_subprocess
 
 
+import importlib
 with AbsoluteImport():
-    import importlib
     subprocess_spec = importlib.util.find_spec("subprocess")
-    subprocess = importlib.util.module_from_spec(subprocess_spec)
-    subprocess_spec.loader.exec_module(subprocess)
-    cur_mod = globals()
-    for k, v in vars(subprocess):
-        if k == "check_output":
-            continue
-        cur_mod[k] = v
-
+subprocess = importlib.util.module_from_spec(subprocess_spec)
+subprocess_spec.loader.exec_module(subprocess)
+cur_mod = globals()
+for k, v in vars(subprocess):
+    if k == "check_output":
+        continue
+    cur_mod[k] = v
 
 
 from req_run import check_output
